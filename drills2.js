0cdedbfc83b9730cf9f7925ae7bb323db4bd131a
Wendy Bartos <wendyebartos@gmail.com>
2019-10-21 12:29:35 -0700

    added infinity function

--- drills2.js
+++ drills2.js
@@ -1,3 +1,19 @@
-function jediName(firstName, LastName){
-    return lastName.slice(0,2) + firstName.slice(0,1);
+/* eslint-disable no-console */
+/* eslint-disable strict */
+function jediName(firstName, lastName){
+  return lastName.slice(0,2) + firstName.slice(0,1);
+}
+function beyond (num) {
+  if (num === Infinity) {
+    console.log('And beyond');
+  }
+  if (num > 0) {
+    console.log('To infinity');
+  }
+  if (num < 0) {
+    console.log('To negative infinity');
+  }
+  if (num === 0) {
+    console.log('Staying home');
+  }
 }
