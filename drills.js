05a91add42bcfc543191711313d4d71265a276e8
Jack <pupeljack@gmail.com>
2019-10-21 13:56:15 -0500

    separated YoB calculation into its own function

--- drills.js
+++ drills.js
@@ -1,8 +1,10 @@
 function createGreeting(name, age){
-    let yearOfBirth = 2019 - age;
-    return `My name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}`;
+    const yob = getYearOfBirth(age);
+    return `My name is ${name} and I am ${age} years old. I was born in ${yob}`;
 }
-
+function getYearOfBirth(age){
+    return 2019 - age;
+}
 
 
 const greeting1 = createGreeting("Jack", 734);
