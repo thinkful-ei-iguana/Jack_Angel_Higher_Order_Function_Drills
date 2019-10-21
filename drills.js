15c295a031088672e9d8eb687e90267b184ea166
Jack <pupeljack@gmail.com>
2019-10-21 13:54:50 -0500

    added a year of birth sentence

--- drills.js
+++ drills.js
@@ -1,6 +1,9 @@
 function createGreeting(name, age){
-    return `My name is ${name} and I am ${age} years old`;
+    let yearOfBirth = 2019 - age;
+    return `My name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}`;
 }
+
+
 
 const greeting1 = createGreeting("Jack", 734);
 console.log(greeting1);
