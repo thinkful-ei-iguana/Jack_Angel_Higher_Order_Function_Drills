5788fbedc6277ed120ff304bc569fc161b2e4efd
Jack <pupeljack@gmail.com>
2019-10-21 14:07:24 -0500

    reviewed code against the cheat sheet

--- drills.js
+++ drills.js
@@ -1,21 +1,27 @@
 function createGreeting(name, age){
-    if(name == null || age == null){
+    if(name === undefined || age === undefined){
         throw new Error("Name or Age not valid");
     }
-    try {
-        const yob = getYearOfBirth(age);
-    } catch {
-        console.log(Error.message);
+    if(typeof(age) != Number){
+        throw new Error("age has to be a number");
     }
-    return `My name is ${name} and I am ${age} years old. I was born in ${yob}`;
-}
-function getYearOfBirth(age){
+    if(typeof(name) != String){
+        throw new Error("name has to be a string");
+    }
     if(age < 0){
         throw new Error("Age cant be negative");
     }
+        const yob = getYearOfBirth(age);
+    return `My name is ${name} and I am ${age} years old. I was born in ${yob}`;
+}
+function getYearOfBirth(age){
+    
     return 2019 - age;
 }
 
-
+try {
 const greeting1 = createGreeting("Jack", 734);
 console.log(greeting1);
+} catch(e){
+    console.error(e.message);
+}
