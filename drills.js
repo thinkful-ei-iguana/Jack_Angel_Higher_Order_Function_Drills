b2c46f6e6e9c14d5b4b9ed693f9506cf42c254a7
Jack <pupeljack@gmail.com>
2019-10-21 14:00:19 -0500

    made negative numbers not allowed

--- drills.js
+++ drills.js
@@ -1,8 +1,15 @@
 function createGreeting(name, age){
-    const yob = getYearOfBirth(age);
+    try{
+        const yob = getYearOfBirth(age);
+    } catch {
+        console.log(Error.message);
+    }
     return `My name is ${name} and I am ${age} years old. I was born in ${yob}`;
 }
 function getYearOfBirth(age){
+    if(age < 0){
+        throw new Error("Age cant be negative");
+    }
     return 2019 - age;
 }
 
