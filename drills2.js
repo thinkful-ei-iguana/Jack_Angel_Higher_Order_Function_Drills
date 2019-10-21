6d662a035cd39619902f3b759b38fd37884c7670
Jack <pupeljack@gmail.com>
2019-10-21 15:12:28 -0500

    made a RSP bot

--- drills2.js
+++ drills2.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable strict */
 function jediName(firstName, lastName){
-<<<<<<< HEAD
     return lastName.slice(0,2) + firstName.slice(0,1);
   }
   function beyond (num) {
@@ -84,41 +83,30 @@ function jediName(firstName, lastName){
   }
 
   
-=======
-  return lastName.slice(0,2) + firstName.slice(0,1);
+function RSP(num){
+    if (!(num <=3) || !(num >= 1)){
+        throw new Error("no cheating");
+    }
+    const randomNo = Math.floor(Math.random() * 3) + 1;
+    if(num === randomNo){
+        console.log("tie");
+    } else if(num === 1){
+        if(randomNo === 2){
+            console.log("loss");
+        } else {
+            console.log("win");
+        }
+    } else if( num === 2){
+        if(randomNo === 1){
+            console.log("win");
+        } else {
+            console.log("loss");
+        }
+    } else {
+        if(randomNo === 1){
+            console.log("loss");
+        } else {
+            console.log("win");
+        }
+    }
 }
-function beyond (num) {
-  if (num === Infinity) {
-    console.log('And beyond');
-  }
-  if (num > 0) {
-    console.log('To infinity');
-  }
-  if (num < 0) {
-    console.log('To negative infinity');
-  }
-  if (num === 0) {
-    console.log('Staying home');
-  }
-}
-
-function decode (word) {
-  if (word.startsWith('a')) {
-    return word.charAt(1);
-  }
-  else if (word.startsWith('b')) {
-    return word.charAt(2);
-  }
-  else if (word.startsWith('c')) {
-    return word.charAt(3);
-  }
-  else if (word.startsWith('d')) {
-    return word.charAt(4);   
-  }
-  else {
-    return ' ';
-  }
-} 
-decode('craft block argon meter bells brown croon droop')
-
->>>>>>> 045e573798efc4a2cae800476fed99de4fa6d939
