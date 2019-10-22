060bc3313368234d6f73a11614893f343527c0e1
Angel Alicea <ala31996@gmail.com>
2019-10-22 14:13:16 -0400

    added my solution for max min drill, also completed the functions as arguements drill

--- Day2Drills.js
+++ Day2Drills.js
@@ -36,6 +36,31 @@ console.log(min([1,2,3]));
 console.log(max([1,2,3]));
 console.log(average([1,2,3]));
 
+//Angel Max-Min Drill Redone
+function maxNumber(numbers) {
+  let currentMax = numbers[0];
+  let i = 0;
+  while ( i < numbers.length) {
+    if (numbers[i] > currentMax) {
+      currentMax = numbers[i];
+    }
+    i++
+  }
+  return currentMax;
+}
+  
+function minNumber(numbers) {
+  let currentMin = numbers[0];
+  let i = 0;
+  while (i < numbers.length) {
+    if (numbers[i] < currentMin) {
+      currentMin = numbers[i];
+    }
+    i++
+  }
+  return currentMin;
+}
+
 
 
 function repeat(fn, n){
@@ -54,3 +79,22 @@ function goodbye(){
   
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+function filter(arr, fn) {
+  let newArray = [];
+  arr.forEach(function (element){
+    if(fn (element)){
+      newArray.push(element);
+    }
+
+  });
+  return newArray;
+}
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames);
